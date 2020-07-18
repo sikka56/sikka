ff7d18bb6c9c914a1a1becc9e67e0eb0a334223d
amit babu <amitsingh506142@gmail.com>
2020-07-18 20:30:58 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter login demo',
+        title: 'sikka',
         debugShowCheckedModeBanner: false,
         theme:  ThemeData(
           primarySwatch: Colors.blue,
