42970981bb2fb19648782fbcef8f791d47da5b82
amit babu <amitsingh506142@gmail.com>
2020-07-18 20:09:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sikka/services/aunthication.dart';
+import 'package:sikka/pages/root_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme:  ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RootPage(auth:Auth()));
+  }
+}
